5260cd1af490d6fc3705c6eadb58ea04e0f3412c
WCurtG <curt@leaent.com>
2021-06-26 02:39:21 -0400

    Update rasax1.sh

--- rasax1.sh
+++ rasax1.sh
@@ -3,7 +3,7 @@
 echo "Starting apt update installing snapd, git, docker, microk8s cluster and Octant manager on your server.." ; 
 sleep 1.5 ;
 sudo apt update ;
-wget https://gist.githubusercontent.com/WCurtG/10273e1fca1c125a7e8bd103c9e9da62/raw/5738f12ad6453404929f48b46cbe0bcb0671cfde/values.yml ;
+wget https://gist.github.com/WCurtG/10273e1fca1c125a7e8bd103c9e9da62 ;
 echo "We have added a values.yml file to you VM you need to update your info aftet this process has completed." ;
 sleep 1.5 ;
 sudo apt install snapd ;
