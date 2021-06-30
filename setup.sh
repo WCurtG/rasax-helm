2c95cfec07323eee8ad8553259acacca87db9122
WCurtG <curt@leaent.com>
2021-06-30 16:15:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,8 @@ do
 
     case "$answer" in 
         y)
-        echo "Great let's proceed!Starting apt update installing snapd, git, docker, microk8s cluster and Octant manager on your server.." && sleep 1
+        echo "Great let's proceed!Starting apt update installing snapd, git, docker, microk8s cluster and Octant manager on your server.." && sleep 1 ;
+        cd $HOME &&
         wget https://gist.githubusercontent.com/WCurtG/10273e1fca1c125a7e8bd103c9e9da62/raw/f5d12bcc4200aa2f9a95782b9a6ac30e4effc603/values.yml &&
         touch .env &&
         echo "EXTERNAL_IPS="$(hostname -I | awk '{print $1}')"" >> .env && 
