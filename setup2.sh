2bd3cdd64d2aa77a54eed2696e996d54dc478c88
WCurtG <curt@leaent.com>
2021-06-30 16:08:32 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-echo "Enabling add ons & Configuring kubectl..."
+echo "------------------Enabling add ons & Configuring kubectl...------------------"
 sleep 2 ;
 cd $HOME/.kube
 microk8s config > config
 microk8s enable dns storage helm3 registry dashboard ingress
-echo "Currently adding aliases to the .bashrc file.." ;
+echo "------------------Currently adding aliases to the .bashrc file..------------------" ;
 sleep 2 ;
 echo "alias kubectl='microk8s.kubectl'" >> ~/.bashrc &&
 echo "alias helm='microk8s.helm3'" >> ~/.bashrc &&
 echo "alias k="kubectl --namespace my-namespace"" >> ~/.bashrc &&
 echo "alias h="helm --namespace my-namespace"" >> ~/.bashrc &&
-source ~/.bashrc && echo ".bashrc has been updated" || echo ".bashrc update has failed" ;
+source ~/.bashrc && echo "------------------.bashrc has been updated------------------" || echo "------------------.bashrc update has failed------------------" ;
 
 cd $HOME &&
 mkdir octant &&
@@ -20,17 +20,17 @@ cd octant &&
 wget https://github.com/vmware-tanzu/octant/releases/download/v0.15.0/octant_0.15.0_Linux-64bit.deb &&
 sudo dpkg -i octant_0.15.0_Linux-64bit.deb &&
 OCTANT_LISTENER_ADDR=0.0.0.0:8002 octant --disable-open-browser && 
-echo "Octant has been installed" || 
-echo "Octant install has failed" ;
+echo "------------------Octant has been installed------------------" || 
+echo "------------------Octant install has failed------------------" ;
 
-echo "Installing Helm and Kubectl" ;
+echo "------------------Installing Helm and Kubectl------------------" ;
 sudo snap install kubectl --classic && kubectl version --client || 
-echo "Kubectl install failed" ;
+echo "------------------Kubectl install failed------------------" ;
 kubectl create namespace my-namespace && 
-echo "kubectl namespace my-namespace has been created" ||
-echo "kubectl namespace my-namespace failed to be created" ;
+echo "------------------kubectl namespace my-namespace has been created------------------" ||
+echo "------------------kubectl namespace my-namespace failed to be created------------------" ;
 sudo snap install helm --classic || 
-echo "Helm install failed" ;
+echo "------------------Helm install failed------------------" ;
 cd rasax-helm ; 
 helm repo add rasa-x https://rasahq.github.io/rasa-x-helm ; 
 helm --namespace my-namespace install --values values.yml my-release rasa-x/rasa-x
