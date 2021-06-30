b4ec59fbd0f3079aa31754c4c50433fc96db18c5
WCurtG <curt@leaent.com>
2021-06-30 16:20:31 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -21,7 +21,7 @@ wget https://github.com/vmware-tanzu/octant/releases/download/v0.15.0/octant_0.1
 sudo dpkg -i octant_0.15.0_Linux-64bit.deb &&
 OCTANT_LISTENER_ADDR=0.0.0.0:8002 octant --disable-open-browser && 
 echo "------------------Octant has been installed Open browser at http://$(hostname -I | awk '{print $1}'):8002------------------" || 
-echo "------------------Octant install has failed------------------" ;
+echo "------------------Octant install has failed------------------" &&
 
 echo "------------------Installing Helm and Kubectl------------------" ;
 sudo snap install kubectl --classic && kubectl version --client || 
