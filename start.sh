c95d76b9c74e9f2c5f34f8da27b69d3ed480c2d9
WCurtG <curt@leaent.com>
2021-07-01 10:13:31 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-EXTERNAL_IP=$(hostname -I | awk '{print $1}')
+EXTERNAL_IP="$(hostname -I | awk '{print $1}')"
 
 
 
 wget https://gist.githubusercontent.com/WCurtG/10273e1fca1c125a7e8bd103c9e9da62/raw/df604572a04301285217f7aaf1201a002634b745/temp_values.yml &&
-echo $EXTERNAL_IPS
-sed "s/EXTERNAL_IP/$(EXTERNAL_IP)/" .temp_values.yml > tmp.yml && 
+sleep 3 &&
+echo $EXTERNAL_IP &&
+sed "s/EXTERNAL_IP/$(EXTERNAL_IP)/" temp_values.yml > tmp.yml && 
 mv tmp.yml values.yml &&
 mv -i values.yml $HOME
