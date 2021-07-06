d49414f6a6b44100cb9583eb972bb0c74d0b2eed
WCurtG <curt@leaent.com>
2021-07-06 13:58:41 -0400

    moved values update to start

--- step2.sh
+++ step2.sh
@@ -32,18 +32,9 @@ sudo dpkg -i octant_0.15.0_Linux-64bit.deb &&
 #EXTERNAL_IP="$(hostname -I | awk '{print $1}')"
 export EXTERNAL_IP="$(curl -s "https://ipinfo.io/json" | jq -r '.ip')" &&
 echo Your VM external ip $EXTERNAL_IP &&
-printf "\n# -------------------------------\n#       EXTERNAL_IP has been added to env variables \n# -------------------------------\n" ||
-printf "\n# -------------------------------\n#       EXTERNAL_IP has failed to be added to env variables \n# -------------------------------\n" &&
-printf "\n# Your VM external ip $EXTERNAL_IP it will be added to your values.yml file \n#" &&
 printf "\n# -------------------------------\n#       Octant has been installed Open browser at http://$EXTERNAL_IP:8002 \n# -------------------------------\n" ||
 printf "\n# -------------------------------\n#       Octant install has failed \n# -------------------------------\n" &&
 sleep 4 &&
-# Replace the EXTERNAL_IP variable on temp_values.yml in the repo rename and move it to the root directory for deployment
-cd rasax-helm-master &&
-sed "s/EXTERNAL_IP/$EXTERNAL_IP/" temp_values.yml > tmp.yml && 
-mv tmp.yml values.yml &&
-mv -i values.yml $HOME && 
-printf "\n# We have updated your temp_values.yml file and renamed it, values.yml file with updated EXTERNAL_IP has been added to your root directory \n" &&
 # We are now creating a kubectl namespace called my-namespace with command kubectl create namespace my-namespace
 # kubectl create namespace my-namespace &&
 kubectl create namespace my-namespace &&
