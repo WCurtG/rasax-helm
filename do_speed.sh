5a589f8d7a1f09a154fe829f85b1f2816f1d7661
WCurtG <curt@leaent.com>
2021-07-19 16:39:40 -0400

    do speed test

---
+++ do_speed.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+better_ping=99999
+better_datacenter=''
+better_datacenter_name=''
+max_ping=${1:-1}
+
+datacenters="speedtest-lon1.digitalocean.com
+			speedtest-sfo1.digitalocean.com
+			speedtest-sfo2.digitalocean.com
+			speedtest-sfo3.digitalocean.com
+			speedtest-ams2.digitalocean.com
+			speedtest-ams3.digitalocean.com
+			speedtest-nyc1.digitalocean.com
+			speedtest-nyc2.digitalocean.com
+			speedtest-nyc3.digitalocean.com
+			speedtest-sgp1.digitalocean.com
+			speedtest-fra1.digitalocean.com
+			speedtest-tor1.digitalocean.com
+			speedtest-blr1.digitalocean.com"
+
+while read -r line;
+do
+	ping_avg=`ping -c $max_ping $line | tail -n1 | cut -d'=' -f2 | cut -d'/' -f2`
+	if [ -z "$ping_avg" ]
+	then
+		echo "Ping ${bold}${line}${normal}: not available"
+		continue
+	fi
+	echo "Ping ${bold}${line}${normal}: $ping_avg ms"
+	if [ $(echo "$ping_avg < $better_ping" | bc) -eq 1 ];
+	then
+		better_ping=$ping_avg
+		better_datacenter=$line
+		better_datacenter_name=`echo $line | cut -d'.' -f1 | cut -d'-' -f2`
+	fi
+done <<< "$datacenters"
+
+echo ""
+echo "${bold}---------------------- Better Datacenter is -----------------------------------${normal}"
+echo "${bold}Domain${normal}: $better_datacenter"
+echo "${bold}Ping AVG${normal}: $better_ping ms"
+echo "${bold}Name${normal}: $better_datacenter_name"
+echo "${bold}-------------------------------------------------------------------------------${normal}"
+echo ""
--- t
+++ t
@@ -1,8 +1,52 @@
 #!/bin/bash
 
-. set_variables.sh
-
 # Exit immediately if one of the commands ends with a non-zero exit code
 set -e
 
-echo $TEST
+bold=$(tput bold)
+blink=$(tput blink)
+
+# Helper functions
+echo_success() {
+  echo -e "\e[1;32m${1}\e[0m"
+}
+
+echo_bold() {
+  echo -e "\e[1m${1}\e[0m"
+}
+
+echo_error() {
+  echo -e "\e[1;31m${1}\e[0m"
+}
+
+fatal() {
+  echo -e "\e[1;31m${1}\e[0m" >&2
+  exit 1
+}
+
+seperator() {
+  echo -e "\n\n\n"
+  yes = | head -n$(($(tput cols) * 1)) | tr -d '\n'
+  center "$1"
+  yes = | head -n$(($(tput cols) * 1)) | tr -d '\n'
+  echo -e "\n\n\n"
+  # use fatal to stop the script
+  $3
+}
+
+center() {
+  COLUMNS=`tput cols` export COLUMNS # Get screen width.
+  "$1" | awk '
+  { spaces = ('$COLUMNS' - length) / 2
+    while (spaces-- > 0) printf (" ")
+    printf
+  }'
+}     
+
+seperator echo_success "this is centered"
+
+
+echo "Curt"
+# echo "${blink} curt"
+# echo "${bold} Curt"
+
