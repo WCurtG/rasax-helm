42b5a2a34093060d4df248939b34936099c91fcb
WCurtG <curt@leaent.com>
2021-07-22 15:51:12 -0400

    Update octant.sh

--- octant.sh
+++ octant.sh
@@ -2,62 +2,41 @@
 
 set -e
 
-# Set debug mode
-boolean() {
-  case $1 in
-  true) echo true ;;
-  false) echo false ;;
-  *)
-    echo "Err: Unknown boolean value \"$1\"" 1>&2
-    exit 1
-    ;;
-  esac
-}
-
-INSTALLER_DEBUG_MODE=$(boolean "${INSTALLER_DEBUG_MODE:-false}")
-
-# Installer debug vars
-if $INSTALLER_DEBUG_MODE; then
-  set +x
-  REDIRECT=/dev/stdout
-else
-  REDIRECT=/dev/null
-fi
-
 # Helper functions
 EXTERNAL_IP=$(curl -s http://whatismyip.akamai.com/)
 
 echo_success() {
-  echo -e "\e[1;32m${1}\e[0m"
+    echo -e "\e[1;32m${1}\e[0m"
 }
 
 echo_bold() {
-  echo -e "\e[1m${1}\e[0m"
+    echo -e "\e[1m${1}\e[0m"
 }
 
 echo_error() {
-  echo -e "\e[1;31m${1}\e[0m"
+    echo -e "\e[1;31m${1}\e[0m"
 }
 
 fatal() {
-  echo "$@" >&2
-  exit 1
+    echo -e "\e[1;31m${1}\e[0m" >&2
+    exit 1
 }
 
-seperator() {
-  echo -e "\n\n\n"
-  yes = | head -n$(($(tput cols) * 1)) | tr -d '\n'
-  printf "\n \n $($1 "$2") \n \n"
-  yes = | head -n$(($(tput cols) * 1)) | tr -d '\n'
-  echo -e "\n\n\n"
-  # use fatal to stop the script
-  $3
-}
+# $(tput cols) && columns=$(tput cols) || columns=100
 
 seperator() {
-  yes '=' | head -n$(($(tput cols) * 1)) | tr -d '\n'
-  printf "\n \n $($1 "$2") \n \n"
-  yes '=' | head -n$(($(tput cols) * 1)) | tr -d '\n'
+    echo -e "\n\n\n"
+    yes = | head -n$((${COLUMNS} * 1)) | tr -d '\n'
+    printf "\n \n \t \t \t $($1 "$2") \n \n"
+    yes = | head -n$((${COLUMNS} * 1)) | tr -d '\n'
+    echo -e "\n\n\n"
+    # use fatal to stop the script
+    $3
+}
+
+line_break() {
+    yes = | head -n$((${COLUMNS} * 1)) | tr -d '\n'
+    echo -e "\n\n\n"
 }
 
 
