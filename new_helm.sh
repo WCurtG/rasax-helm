d427f9f7acf17b547b3338a0da76260c56d82024
WCurtG <curt@leaent.com>
2021-07-22 15:32:25 -0400

    Update new_helm.sh

--- new_helm.sh
+++ new_helm.sh
@@ -65,6 +65,7 @@ fatal() {
     exit 1
 }
 
+# Causes an error on Github Actions because tput is not available
 $(tput cols) && columns=$(tput cols) || columns=100
 
 seperator() {
@@ -78,76 +79,8 @@ seperator() {
 }
 
 line_break() {
-    yes = | head -n$(($columns * 1)) | tr -d '\n'
+    yes = | head -n$((${columns} * 1)) | tr -d '\n'
     echo -e "\n\n\n"
-}
-
-# Loading animation while the new rasa x is being deployed
-
-# wait_for_rasa_x_deployment() {
-#   # Use `/dev/null` for everything since the expected timeout will be logged to `stderr`
-#   microk8s.kubectl wait --namespace "${NAME_SPACE}" --for=condition=available --timeout=10s -l "app.kubernetes.io/component=rasa-x" deployment & > /dev/null
-# }
-
-# wait_for_deployment_to_be_healthy() {
-#   # Get the Rasa X pod name
-#   POD=$(microk8s.kubectl --namespace "${NAME_SPACE}" get pod -l app.kubernetes.io/component=rasa-x -o name)
-
-#   # shellcheck disable=SC2016
-#   # Check the Rasa X health endpoint to be sure that the deployment is ready and the Rasa X is fully operational
-#   # The Rasa X health endpoints returns status 200 if the rasa-production and rasa-worker services are ready
-#   # The endpoint is checked inside of the rasa x pod in order to avoid dependency on an ingress configuration
-#   microk8s.kubectl --namespace "${NAME_SPACE}" exec "${POD}" -- /bin/bash -c 'curl -s localhost:$SELF_PORT/api/health | grep "\"status\":200"' &> /dev/null
-# }
-
-run_loading_animation() {
-    i=1
-    sp="/-\|"
-    while :; do
-        sleep 0.1
-        # Don't show spinner when we are debugging
-        if ! $INSTALLER_DEBUG_MODE; then
-            printf "\b%s" ${sp:i++%${#sp}:1}
-        fi
-    done
-}
-
-wait_till_deployment_finished() {
-  # Run the loading animation in the background while are waiting for the deployment
-  run_loading_animation &
-  LOADING_ANIMATION_PID=$!
-  # Kill loading animation when the install script is killed
-  # Also mute error output in case the process was already killed before
-  # shellcheck disable=SC2064
-  trap "kill -9 ${LOADING_ANIMATION_PID} &> /dev/null || true" $(seq 1 15)
-
-#   # Wait until the Rasa deployment is up and running
-#   while ! wait_for_rasa_x_deployment; do
-#     microk8s.kubectl --namespace "${NAME_SPACE}" get pod > /dev/null
-#   done
-
-#   # Wait until Rasa X is fully operational
-#   while ! wait_for_deployment_to_be_healthy; do
-#     sleep 5
-#   done
-
-  # Curt Test
-  ns_status=$(microk8s.kubectl get pods --field-selector=status.phase!=Succeeded,status.phase!=Running --namespace "${NAME_SPACE}")
-  echo_success "Namespace "${NAME_SPACE}"is installing...."
-  while [ ${#ns_status} -ne 0 ]; 
-  do
-    ns_status=$(microk8s.kubectl get pods --field-selector=status.phase!=Succeeded,status.phase!=Running --namespace "${NAME_SPACE}")
-     sleep 1 
-  done
-  echo_success "Namespace "${NAME_SPACE}"status: Active" &&
-  microk8s.kubectl get pods --namespace "${NAME_SPACE}"
-  POD=$(microk8s.kubectl --namespace "${NAME_SPACE}" get pod -l app.kubernetes.io/component=rasa-x -o name)
-  microk8s.kubectl --namespace "${NAME_SPACE}" exec "${POD}" -- /bin/bash -c 'curl -s localhost:$SELF_PORT/api/health | grep "\"status\":200"'
-  # Stop the loading animation since the deployment is finished
-  kill -9 ${LOADING_ANIMATION_PID}
-
-  # Remove remnants of the spinner
-  printf "\b"
 }
 
 download() {
@@ -295,15 +228,6 @@ rebuild_namespace() {
     password_question
 }
 
-# upgrade_namespace() {
-#     line_break &&
-#         read -e -p "Would you like to upgrade "${NAME_SPACE}"? [Y/n] " YN
-#     [[ $YN == "y" || $YN == "Y" || $YN == "" ]] &&
-#         cd "${HOME}" &&
-#         microk8s.helm3 --namespace "${NAME_SPACE}"upgrade --values values.yml my-release rasa-x/rasa-x ||
-#         seperator echo_error "namespace "${NAME_SPACE}"has not been upgraded. Exiting.." fatal
-# }
-
 password_question() {
     INITIAL_USERNAME=${INITIAL_USERNAME:-me} &&
     INITIAL_USER_PASSWORD=${INITIAL_USER_PASSWORD} &&
@@ -350,6 +274,8 @@ deploy_helm() {
         microk8s.helm3 --namespace "${NAME_SPACE}" install --values values.yml my-release rasa-x/rasa-x &&
         seperator echo_success "microk8s.helm3 --namespace "${NAME_SPACE}" using values.yml has been installed" ||
         seperator echo_error "microk8s.helm3 --namespace "${NAME_SPACE}" install Failed" fatal
+
+        # Waiting for the deployment to be ready
         wait_till_deployment_finished &&
         echo_success "Open in your browser here http://${EXTERNAL_IP}:8000/api/version to check the api status and version \n \n Or run this command in your cli \n \n microk8s.kubectl --namespace "${NAME_SPACE}" get services && curl http://${EXTERNAL_IP}/api/version" &&
         microk8s.kubectl get pods --namespace "${NAME_SPACE}" &&
@@ -357,9 +283,48 @@ deploy_helm() {
         provide_login_credentials
 }
 
+run_loading_animation() {
+    i=1
+    sp="/-\|"
+    while :; do
+        sleep 0.1
+        # Don't show spinner when we are debugging
+        if ! $INSTALLER_DEBUG_MODE; then
+            printf "\b%s" ${sp:i++%${#sp}:1}
+        fi
+    done
+}
+
+wait_till_deployment_finished() {
+  # Run the loading animation in the background while are waiting for the deployment
+  run_loading_animation &
+  LOADING_ANIMATION_PID=$!
+  # Kill loading animation when the install script is killed
+  # Also mute error output in case the process was already killed before
+  # shellcheck disable=SC2064
+  trap "kill -9 ${LOADING_ANIMATION_PID} &> /dev/null || true" $(seq 1 15)
+  # Wait for the deployment to be ready   
+  ns_status=$(microk8s.kubectl get pods --field-selector=status.phase!=Succeeded,status.phase!=Running --namespace "${NAME_SPACE}")
+  echo_success "Namespace "${NAME_SPACE}" is installing...."
+  while [ ${#ns_status} -ne 0 ]; 
+  do
+    ns_status=$(microk8s.kubectl get pods --field-selector=status.phase!=Succeeded,status.phase!=Running --namespace "${NAME_SPACE}")
+     sleep 1 
+  done
+  echo_success "Namespace "${NAME_SPACE}"status: Active" &&
+  microk8s.kubectl get pods --namespace "${NAME_SPACE}"
+  POD=$(microk8s.kubectl --namespace "${NAME_SPACE}" get pod -l app.kubernetes.io/component=rasa-x -o name)
+  microk8s.kubectl --namespace "${NAME_SPACE}" exec "${POD}" -- /bin/bash -c 'curl -s localhost:$SELF_PORT/api/health | grep "\"status\":200"'
+  # Stop the loading animation since the deployment is finished
+  kill -9 ${LOADING_ANIMATION_PID}
+
+  # Remove remnants of the spinner
+  printf "\b"
+}
+
 provide_login_credentials() {
     # Explain how to access Rasa X
-    echo -e "The deployment is ready 🤖. "
+    echo_success "The deployment is ready 🤖. "
 
     # Determine the public IP address
     PUBLIC_IP=$(curl -s http://whatismyip.akamai.com/)
