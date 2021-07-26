3da7ca7160874d908780e13f5c22b2eeb4747fb7
WCurtG <curt@leaent.com>
2021-07-26 13:36:23 -0400

    test

--- scripts/Utils/test.sh
+++ scripts/Utils/test.sh
@@ -58,36 +58,6 @@ seperator() {
     $3
 }
 
-# This is the easist way to add the Repo to your VM
+branch="refs/head/restructer"
 
-app_installed() {
-    # Return failure if it doesn't exist or is no executable
-    which "$1" >/dev/null || retun 1
-
-    return 0
-}
-
-download () {
-  app_installed rasax-helm && 
-  ( sudo apt update -q && sudo apt upgrade -qy && sudo apt autoremove -qy &&
-  wget https://github.com/WCurtG/rasax-helm/archive/refs/heads/master.zip >${REDIRECT} &&
-  sudo apt-get install unzip -y &&
-  unzip master.zip >${REDIRECT} && 
-  mv -f rasax-helm-master rasax-helm && 
-  clean_up ) || fatal "rasax-helm is already installed"
-}
-
-clean_up () {
-  find $PWD -type f -iname "master.zip" -mmin -2 -delete &&
-  echo -e "\n\n\n"
-  echo_success "Cleaning up your download...."
-  echo -e "\n\n\n"
-  sleep 3
-}
-
-app_installed rasax-helm
-echo_success "Rasax-Helm is already installed"
-
-# download &&
-# seperator echo_success "master.zip has been cleaned from root and rasa-helm-master been added. You are currently in root/rasa-helm-master"
-
+echo "${branch##*/}"
