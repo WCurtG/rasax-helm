717139c44583bcc6d9d70820caef92818a5d1cac
WCurtG <curt@leaent.com>
2021-09-22 10:14:09 -0400

    corrections

--- scripts/Utils/test.sh
+++ scripts/Utils/test.sh
@@ -2,65 +2,12 @@
 
 set -e
 
-# Set debug mode
-boolean() {
-  case $1 in
-  true) echo true ;;
-  false) echo false ;;
-  *)
-    echo "Err: Unknown boolean value \"$1\"" 1>&2
-    exit 1
-    ;;
-  esac
+has() {
+  command -v "$1" 1>/dev/null 2>&1
 }
 
-INSTALLER_DEBUG_MODE=$(boolean "${INSTALLER_DEBUG_MODE:-false}")
-
-# Installer debug vars
-if $INSTALLER_DEBUG_MODE; then
-  set +x
-  REDIRECT=/dev/stdout
+if has brew; then
+    echo brew is installed
 else
-  REDIRECT=/dev/null
+    echo brew is not installed
 fi
-
-# Helper functions
-echo_success() {
-  echo -e "\e[1;32m${1}\e[0m"
-}
-
-echo_bold() {
-  echo -e "\e[1m${1}\e[0m"
-}
-
-echo_error() {
-  echo -e "\e[1;31m${1}\e[0m"
-}
-
-fatal() {
-  echo "$@" >&2
-  exit 1
-}
-
-
-seperator() {
-  echo -e "\n\n\n"
-  yes '=' | head -n$((${COLUMNS} * 1)) | tr -d '\n'
-  printf "\n \n \t \t \t $($1 "$2") \n \n"
-  yes '=' | head -n$(($COLUMNS * 1)) | tr -d '\n'
-  echo -e "\n\n\n"
-  # use fatal to stop the script
-  $3
-}
-
-# echo $COLUMNS
-
-# seperator echo_success "testin"
-
-FB_CALL=2
-call_fb() {
-  ((${FB_CALL}>5)) && echo "Get FB info update global vars" || echo "Don't get FB info"
-}
-
-echo ${FB_CALL}
-call_fb
